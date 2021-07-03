9c93e2d3a97adffe50bc01ed10dded88f4ac4d60
tian <istiancz@gmail.com>
2021-07-03 21:11:30 +0800

    generate graph

--- minipack.js
+++ minipack.js
@@ -1,9 +1,10 @@
 const fs = require("fs");
+const path = require("path");
 const { parse } = require("@babel/parser");
 const traverse = require("@babel/traverse").default;
 
 let ID = 0;
-const cteateAsset = (path) => {
+const createAsset = (path) => {
   const code = fs.readFileSync(path, "utf-8");
   const ast = parse(code, {
     sourceType: "module",
@@ -22,16 +23,21 @@ const cteateAsset = (path) => {
   };
 };
 
-const createGraph = (path) => {
-  const mainAssets = cteateAsset("./src/entry.js");
+const createGraph = (entry) => {
+  const mainAssets = createAsset(entry);
   const assets = [mainAssets];
   for (const asset of assets) {
     const { deps } = asset;
-    const dirName = path.dirName(asset.path);
+    asset.mapping = {};
+    const dir = path.dirname(asset.path);
     for (const dep of deps) {
-      const child = createAsset(dep);
+      const absPath = path.join(dir, dep); // 'src\world' shit
+      const c = `./${absPath.replace(/\\/g, "/")}`;
+      const child = createAsset(c);
+      asset.mapping[dep] = child.id;
       assets.push(child);
     }
   }
+  return assets;
 };
-createGraph("./src/entry.js");
+console.log(createGraph("./src/entry.js"));
